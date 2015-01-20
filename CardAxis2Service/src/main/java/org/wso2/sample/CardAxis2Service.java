3ea6ee41e09d4fb1e1d40a755aba0a4cb0921355
Enzo Cappa <ecappa@belatrixsf.com>
2015-01-20 14:59:37 -0300

    Better card service

--- CardAxis2Service/src/main/java/org/wso2/sample/CardAxis2Service.java
+++ CardAxis2Service/src/main/java/org/wso2/sample/CardAxis2Service.java
@@ -2,19 +2,15 @@ package org.wso2.sample;
 
 public class CardAxis2Service {
 	private int cardNo = 1000;
-//	private String existing = "1";
 	private String nonexisting = "0";
 	
 	public Card issueCard(String cID, String customerExists, String cName, String accntNo){
 		Card card = new Card(cID);
+		card.setCustomerName(cName);
 		if(customerExists.equals(nonexisting)){
-		    card.setAccountNo(accntNo);
-		    card.setCustomerName(cName);
+		    card.setAccountNo(accntNo);		    
 		    card.setCardNo(cardNo++);
 		    card.setPin((int)(Math.random()*1000));
-		}else{
-			card.setCustomerID(cID);
-			card.setCustomerName(cName);
 		}
 		return card;
 	}
